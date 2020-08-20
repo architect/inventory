ca454a7c24a04e9074d513684f4a2c5b4f9892bb
Ryan Block <rblock@gmail.com>
2020-08-20 14:35:21 -0700

    Expand scheduled.lambda.rate|cron to include detailed params

--- src/config/pragmas/populate-lambda/_scheduled.js
+++ src/config/pragmas/populate-lambda/_scheduled.js
@@ -1,5 +1,33 @@
 let { join } = require('path')
 
+let coerceNumbers = str => !isNaN(Number(str)) ? Number(str) : str
+
+function getRate (expression) {
+  // TODO validation
+  let bits = expression.split(' ')
+  bits = bits.map(coerceNumbers)
+  return {
+    expression,
+    value: bits[0],
+    interval: bits[1]
+  }
+}
+
+function getCron (expression) {
+  // TODO validation
+  let bits = expression.split(' ')
+  bits = bits.map(coerceNumbers)
+  return {
+    expression,
+    minutes: bits[0],
+    hours: bits[1],
+    dayOfMonth: bits[2],
+    month: bits[3],
+    dayOfWeek: bits[4],
+    year: bits[5],
+  }
+}
+
 module.exports = function populateScheduled ({ item, dir, cwd }) {
   let rate = null
   let cron = null
@@ -10,8 +38,8 @@ module.exports = function populateScheduled ({ item, dir, cwd }) {
     let sched = item.slice(1).join(' ').split('(')
     let schedType = sched[0]
     let schedValue = sched[1].replace(')', '')
-    if (schedType === 'rate') rate = schedValue
-    if (schedType === 'cron') cron = schedValue
+    if (schedType === 'rate') rate = getRate(schedValue)
+    if (schedType === 'cron') cron = getCron(schedValue)
 
     let src = join(cwd, dir, name)
     return { name, src, rate, cron }
@@ -20,8 +48,8 @@ module.exports = function populateScheduled ({ item, dir, cwd }) {
     let name = Object.keys(item)[0]
 
     // Handle rate + cron
-    if (item[name].rate) rate = item[name].rate.join(' ')
-    if (item[name].cron) cron = item[name].cron.join(' ')
+    if (item[name].rate) rate = getRate(item[name].rate.join(' '))
+    if (item[name].cron) cron = getCron(item[name].cron.join(' '))
 
     let src = item[name].src
       ? join(cwd, item[name].src)
