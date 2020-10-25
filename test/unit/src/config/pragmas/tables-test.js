0062a9d0fca61a01e8df9c4a5c660d064b4c5232
Ryan Block <rblock@gmail.com>
2020-10-25 13:52:26 -0700

    Add additional params to @tables

--- test/unit/src/config/pragmas/tables-test.js
+++ test/unit/src/config/pragmas/tables-test.js
@@ -43,6 +43,40 @@ number-keys
   t.equal(tables[1].stream, true, 'Got back correct stream value for second table')
 })
 
+test('@tables population (extra params)', t => {
+  t.plan(14)
+
+  let arc = parse(`
+@tables
+string-keys
+  strID *String
+  strSort **String
+  stream true
+  _ttl TTL
+  PointInTimeRecovery true
+  encrypt true
+  legacy true
+  insert Lambda # Legacy param (ignored)
+  update Lambda # Legacy param (ignored)
+  delete Lambda # Legacy param (ignored)
+`)
+  let tables = populateTables({ arc })
+  t.ok(tables.length === 1, 'Got correct number of tables back')
+  t.equal(tables[0].name, 'string-keys', 'Got back correct name')
+  t.equal(tables[0].partitionKey, 'strID', 'Got back correct partition key')
+  t.equal(tables[0].partitionKeyType, 'String', 'Got back correct partition key type')
+  t.equal(tables[0].sortKey, 'strSort', 'Got back correct sort key')
+  t.equal(tables[0].sortKeyType, 'String', 'Got back correct sort key type')
+  t.equal(tables[0].stream, true, 'Got back correct stream value')
+  t.equal(tables[0].ttl, '_ttl', 'Got back correct TTL value')
+  t.equal(tables[0].PointInTimeRecovery, true, 'Got back correct PointInTimeRecovery value')
+  t.equal(tables[0].encrypt, true, 'Got back correct encrypt value')
+  t.equal(tables[0].legacy, true, 'Got back correct legacy value')
+  t.equal(tables[0].insert, undefined, 'Skipped deprecated insert Lambda setting')
+  t.equal(tables[0].update, undefined, 'Skipped deprecated update Lambda setting')
+  t.equal(tables[0].delete, undefined, 'Skipped deprecated delete Lambda setting')
+})
+
 test('@tables population: invalid tables throw', t => {
   t.plan(1)
   let arc = parse(`
