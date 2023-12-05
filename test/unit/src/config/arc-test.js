6d249b6793220a775b0fa8334ad0e957698724b3
Ryan Block <rblock@gmail.com>
2023-12-05 11:12:14 -0800

    Update arc version unit tests to use `mock-tmp`

--- test/unit/src/config/arc-test.js
+++ test/unit/src/config/arc-test.js
@@ -1,5 +1,5 @@
 let { join } = require('path')
-let mockFs = require('mock-fs')
+let mockTmp = require('mock-tmp')
 let test = require('tape')
 let sut = join(process.cwd(), 'src', 'config', 'arc')
 let getArcConfig = require(sut)
@@ -11,12 +11,11 @@ test('Set up env', t => {
 
 test('Set Arc version (if possible)', t => {
   t.plan(1)
-  let cwd = process.cwd()
-  let path = join(cwd, 'node_modules', '@architect', 'architect', 'package.json')
+  let path = join('node_modules', '@architect', 'architect', 'package.json')
   let version = 'lol'
   let json = JSON.stringify({ version })
-  mockFs({ [path]: json })
+  let cwd = mockTmp({ [path]: json })
   let arc = getArcConfig({ cwd, inventory: { _arc: {} } })
   t.equal(arc.version, version, 'Got back installed arc version')
-  mockFs.restore()
+  mockTmp.reset()
 })
