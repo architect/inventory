365deadfe761458bc9562db783e26413738c3933
filmaj <maj.fil@gmail.com>
2025-04-19 08:59:43 -0400

    chore: moving to node native test runner
    gitignore updates for node test runner
    
    split out run scripts for integration tests on node 20 vs 22, to account for node test runner CLI differences

--- test/integration/preferences-test.js
+++ test/integration/preferences-test.js
@@ -1,11 +1,9 @@
-let { join } = require('path')
-let test = require('tape')
+let { join } = require('node:path')
+let { test } = require('node:test')
 let mockTmp = require('mock-tmp')
 let cwd = process.cwd()
-let testLibPath = join(cwd, 'test', 'lib')
-let { overrideHomedir } = require(testLibPath)
-let sut = join(cwd, 'src', 'index')
-let inv = require(sut)
+let { overrideHomedir } = require('../lib')
+let inventory = require('../..')
 
 let mock = join(cwd, 'test', 'mock')
 let globalPrefsFile = '.prefs.arc'
@@ -14,16 +12,18 @@ let reset = () => {
   overrideHomedir.reset()
 }
 
+test.beforeEach(reset)
+
 /**
  * Duplicates some unit tests as part of the larger whole integration test
  * See: test/unit/src/config/project/preferences-test.js
  */
 test('Set up env', t => {
   t.plan(1)
-  t.ok(inv, 'Inventory entry is present')
+  t.assert.ok(inventory, 'Inventory entry is present')
 })
 
-test('Get global preferences', t => {
+test('Get global preferences', async t => {
   t.plan(11)
   let cwd = join(mock, 'prefs', 'global')
   let prefs = {
@@ -69,30 +69,25 @@ testing
     [globalPrefsFile]: prefsText,
   })
   overrideHomedir(tmp)
-  inv({ cwd }, (err, result) => {
-    if (err) t.fail(err)
-    else {
-      let { inv, get } = result
-      t.ok(inv, 'Inventory returned inventory object')
-      t.ok(get, 'Inventory returned getter')
-      t.ok(inv._project.preferences, 'Got preferences')
-      t.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
-      t.ok(inv._project.globalPreferences, 'Got globalPreferences')
-      t.ok(inv._project.globalPreferences._arc, 'Got globalPreferences (arc object)')
-      t.ok(inv._project.globalPreferences._raw, 'Got globalPreferences (raw file)')
-      t.notOk(inv._project.localPreferences, 'Did not get localPreferences')
-      t.notOk(inv._project.localPreferencesFile, 'Did not get localPreferencesFile')
-      // Delete the meta stuff so the actual preferences match the above
-      delete inv._project.globalPreferences._arc
-      delete inv._project.globalPreferences._raw
-      t.deepEqual(inv._project.globalPreferences, prefs, 'Got correct global preferences')
-      t.equal(inv._project.globalPreferencesFile, join(tmp, globalPrefsFile), 'Got correct preferences file')
-      t.teardown(reset)
-    }
-  })
+  let result = await inventory({ cwd })
+  let { inv, get } = result
+  t.assert.ok(inv, 'Inventory returned inventory object')
+  t.assert.ok(get, 'Inventory returned getter')
+  t.assert.ok(inv._project.preferences, 'Got preferences')
+  t.assert.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
+  t.assert.ok(inv._project.globalPreferences, 'Got globalPreferences')
+  t.assert.ok(inv._project.globalPreferences._arc, 'Got globalPreferences (arc object)')
+  t.assert.ok(inv._project.globalPreferences._raw, 'Got globalPreferences (raw file)')
+  t.assert.ok(!inv._project.localPreferences, 'Did not get localPreferences')
+  t.assert.ok(!inv._project.localPreferencesFile, 'Did not get localPreferencesFile')
+  // Delete the meta stuff so the actual preferences match the above
+  delete inv._project.globalPreferences._arc
+  delete inv._project.globalPreferences._raw
+  t.assert.deepEqual(inv._project.globalPreferences, prefs, 'Got correct global preferences')
+  t.assert.equal(inv._project.globalPreferencesFile, join(tmp, globalPrefsFile), 'Got correct preferences file')
 })
 
-test('Get local preferences', t => {
+test('Get local preferences', async t => {
   t.plan(11)
   let cwd = join(mock, 'max')
   let prefs = {
@@ -116,31 +111,26 @@ test('Get local preferences', t => {
       production: { 'env_var_1': 'qix qix', 'env_var_2': 'qux qux' },
     },
   }
-  inv({ cwd }, (err, result) => {
-    if (err) t.fail(err)
-    else {
-      let { inv, get } = result
-      t.ok(inv, 'Inventory returned inventory object')
-      t.ok(get, 'Inventory returned getter')
-      t.ok(inv._project.preferences, 'Got preferences')
-      t.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
-      t.ok(inv._project.localPreferences, 'Got localPreferences')
-      t.ok(inv._project.localPreferences._arc, 'Got localPreferences (arc object)')
-      t.ok(inv._project.localPreferences._raw, 'Got localPreferences (raw file)')
-      t.notOk(inv._project.globalPreferences, 'Did not get globalPreferences')
-      t.notOk(inv._project.globalPreferencesFile, 'Did not get globalPreferencesFile')
-      // Delete the meta stuff so the actual preferences match the above
-      delete inv._project.localPreferences._arc
-      delete inv._project.localPreferences._raw
-      t.deepEqual(inv._project.localPreferences, prefs, 'Got correct local preferences')
-      t.equal(inv._project.localPreferencesFile, join(cwd, 'preferences.arc'), 'Got correct preferences file')
-      t.teardown(reset)
-    }
-  })
+  let result = await inventory({ cwd })
+  let { inv, get } = result
+  t.assert.ok(inv, 'Inventory returned inventory object')
+  t.assert.ok(get, 'Inventory returned getter')
+  t.assert.ok(inv._project.preferences, 'Got preferences')
+  t.assert.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
+  t.assert.ok(inv._project.localPreferences, 'Got localPreferences')
+  t.assert.ok(inv._project.localPreferences._arc, 'Got localPreferences (arc object)')
+  t.assert.ok(inv._project.localPreferences._raw, 'Got localPreferences (raw file)')
+  t.assert.ok(!inv._project.globalPreferences, 'Did not get globalPreferences')
+  t.assert.ok(!inv._project.globalPreferencesFile, 'Did not get globalPreferencesFile')
+  // Delete the meta stuff so the actual preferences match the above
+  delete inv._project.localPreferences._arc
+  delete inv._project.localPreferences._raw
+  t.assert.deepEqual(inv._project.localPreferences, prefs, 'Got correct local preferences')
+  t.assert.equal(inv._project.localPreferencesFile, join(cwd, 'preferences.arc'), 'Got correct preferences file')
 })
 
 
-test('Layer local preferences over global preferences', t => {
+test('Layer local preferences over global preferences', async t => {
   t.plan(14)
   let cwd = join(mock, 'prefs', 'local-over-global')
   let globalPrefsText = `
@@ -200,32 +190,27 @@ testing
     [globalPrefsFile]: globalPrefsText,
   })
   overrideHomedir(tmp)
-  inv({ cwd }, (err, result) => {
-    if (err) t.fail(err)
-    else {
-      let { inv, get } = result
-      t.ok(inv, 'Inventory returned inventory object')
-      t.ok(get, 'Inventory returned getter')
-      t.ok(inv._project.preferences, 'Got preferences')
-      t.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
-      t.ok(inv._project.globalPreferences, 'Got globalPreferences')
-      t.ok(inv._project.globalPreferences._arc, 'Got globalPreferences (arc object)')
-      t.ok(inv._project.globalPreferences._raw, 'Got globalPreferences (raw file)')
-      t.ok(inv._project.localPreferences, 'Got localPreferences')
-      t.ok(inv._project.localPreferences._arc, 'Got localPreferences (arc object)')
-      t.ok(inv._project.localPreferences._raw, 'Got localPreferences (raw file)')
-      // Delete the meta stuff so the actual preferences match the above
-      delete inv._project.globalPreferences._arc
-      delete inv._project.globalPreferences._raw
-      delete inv._project.localPreferences._arc
-      delete inv._project.localPreferences._raw
-      t.deepEqual(inv._project.globalPreferences, globalPrefs, 'Got correct global preferences')
-      t.deepEqual(inv._project.localPreferences, localPrefs, 'Got correct local preferences')
-      t.equal(inv._project.globalPreferencesFile, join(tmp, globalPrefsFile), 'Got correct preferences file')
-      t.equal(inv._project.localPreferencesFile, join(cwd, 'preferences.arc'), 'Got correct preferences file')
-      t.teardown(reset)
-    }
-  })
+  let result = await inventory({ cwd })
+  let { inv, get } = result
+  t.assert.ok(inv, 'Inventory returned inventory object')
+  t.assert.ok(get, 'Inventory returned getter')
+  t.assert.ok(inv._project.preferences, 'Got preferences')
+  t.assert.deepEqual(inv._project.preferences, prefs, 'Got correct preferences')
+  t.assert.ok(inv._project.globalPreferences, 'Got globalPreferences')
+  t.assert.ok(inv._project.globalPreferences._arc, 'Got globalPreferences (arc object)')
+  t.assert.ok(inv._project.globalPreferences._raw, 'Got globalPreferences (raw file)')
+  t.assert.ok(inv._project.localPreferences, 'Got localPreferences')
+  t.assert.ok(inv._project.localPreferences._arc, 'Got localPreferences (arc object)')
+  t.assert.ok(inv._project.localPreferences._raw, 'Got localPreferences (raw file)')
+  // Delete the meta stuff so the actual preferences match the above
+  delete inv._project.globalPreferences._arc
+  delete inv._project.globalPreferences._raw
+  delete inv._project.localPreferences._arc
+  delete inv._project.localPreferences._raw
+  t.assert.deepEqual(inv._project.globalPreferences, globalPrefs, 'Got correct global preferences')
+  t.assert.deepEqual(inv._project.localPreferences, localPrefs, 'Got correct local preferences')
+  t.assert.equal(inv._project.globalPreferencesFile, join(tmp, globalPrefsFile), 'Got correct preferences file')
+  t.assert.equal(inv._project.localPreferencesFile, join(cwd, 'preferences.arc'), 'Got correct preferences file')
 })
 
 test('Preferences validation errors', async t => {
@@ -239,11 +224,5 @@ foo
     'app.arc': arc,
     'prefs.arc': prefs,
   })
-  try {
-    await inv({ cwd })
-    t.fail('Expected an error')
-  }
-  catch (err) {
-    t.match(err.message, /Invalid preferences setting: @env foo/, 'Got back error message for invalid preferences')
-  }
+  t.assert.rejects(inventory({ cwd }), /Invalid preferences setting: @env foo/, 'Got back error message for invalid preferences')
 })
